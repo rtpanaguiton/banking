5a348f355aac408332fffce9ff013799e0a9090a
Reymundo Panaguiton <rtpanaguiton@yahoo.com>
2024-07-07 17:39:37 +0800

    add Doughnut chart

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -1,34 +1,33 @@
 "use client";
 
-const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
-  const data = {
-    // labels: accounts.map((account) => account.accountName),
-    datasets: [
-      {
-        label: "Banks",
-        data: [1250, 2500, 3750],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-        labels: ["Savings", "Checking", "Credit Card"],
-      },
-    ],
-  };
-  return <DoughnutChart data={data} />;
-};
+// const DoughnutChart = () => {
+//   const adata = {
+//     datasets: [
+//       {
+//         label: "Banks",
+//         data: [1250, 2500, 3750],
+//         backgroundColor: [
+//           "rgba(255, 99, 132, 0.2)",
+//           "rgba(54, 162, 235, 0.2)",
+//           "rgba(255, 206, 86, 0.2)",
+//           "rgba(75, 192, 192, 0.2)",
+//           "rgba(153, 102, 255, 0.2)",
+//           "rgba(255, 159, 64, 0.2)",
+//         ],
+//         borderColor: [
+//           "rgba(255, 99, 132, 1)",
+//           "rgba(54, 162, 235, 1)",
+//           "rgba(255, 206, 86, 1)",
+//           "rgba(75, 192, 192, 1)",
+//           "rgba(153, 102, 255, 1)",
+//           "rgba(255, 159, 64, 1)",
+//         ],
+//         borderWidth: 1,
+//         labels: ["Savings", "Checking", "Credit Card"],
+//       },
+//     ],
+//   };
+//   return <DoughnutChart />;
+// };
 
-export default DoughnutChart;
+// export default DoughnutChart;
--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,8 +1,6 @@
-import { formatAmount } from "@/lib/utils";
 import React from "react";
-import CountUp from "react-countup";
 import AnimatedCounter from "./AnimatedCounter";
-import DoughnutChart from "./DoughnutChart";
+// import DoughnutChart from "./DoughnutChart";
 
 const TotalBalanceBox = ({
   accounts = [],
@@ -11,16 +9,14 @@ const TotalBalanceBox = ({
 }: TotalBalanceBoxProps) => {
   return (
     <section className="total-balance">
-      <div className="total_balance-chart">
-        <DoughnutChart accounts={accounts} />
-      </div>
+      <div className="total_balance-chart">{/* <DoughnutChart /> */}</div>
       <div className="flex flex-col gap-6">
         <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
         <div className="flex flex-col gap-2">
           <p className="total-balance-label">Total Current Balance</p>
-          <p className="total-balance-amount flex-center gap-2">
+          <div className="total-balance-amount flex-center gap-2">
             <AnimatedCounter amount={totalCurrentBalance} />
-          </p>
+          </div>
         </div>
       </div>
     </section>
